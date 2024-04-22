633888b04e750cce07f25fabbb4304fa1e2d77b2
Lisette Svan <svanlisette@gmail.com>
2024-04-22 10:13:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     my website
+     
+    </>
+  )
+}
+
+export default App
